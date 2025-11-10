7bea1399be8faf130527038d912b4df17736fe4d
fajukind <fajukind@faps.uni-erlangen.de>
2025-11-10 12:32:25 +0100

    BugFix: Add apply_random_augmentation to exports in utils module

--- uav_mission_env/utils/__init__.py
+++ uav_mission_env/utils/__init__.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 from .media_utils import load_and_encode_image, load_image_from_path, pil_image_to_base64_str, base64_str_to_pil_image
-
+from .augmentations_utils import apply_random_augmentation
 __all__ = [
     "load_and_encode_image",
     "load_image_from_path",
     "pil_image_to_base64_str",
     "base64_str_to_pil_image",
+    "apply_random_augmentation"
 ]
